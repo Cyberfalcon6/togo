d971a97fff2e34592550a6d1eb9077cc3e596dc7
CyberFalcon <cyberfalcon660066@gmail.com>
2025-03-17 15:31:19 +0200

    initial commit

--- pyfun.py
+++ pyfun.py
@@ -2,15 +2,3 @@
 print("please give us your text: ", end="")
 txt = input()
 print(len(txt))
-
-"""version two of the counter"""
-# print("please give us your text: ", end="")
-# txt = input()
-# txt1=txt.replace(" ", "")
-# print(len(txt))
-# print(len(txt1))
-
-"""doing it in one line"""
-print("please give us your text: ", end="")
-txt = input()
-print(len(txt.replace(" ", "")))
