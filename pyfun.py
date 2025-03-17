89a1691c8f59995115221ddb11207df0b5b4ef62
CyberFalcon <cyberfalcon660066@gmail.com>
2025-03-17 15:43:20 +0200

    third version

--- pyfun.py
+++ pyfun.py
@@ -3,7 +3,14 @@
 # txt = input()
 # print(len(txt))
 
-"""doing it in one line"""
-print("please give us your text: ", end="")
-txt = input()
-print(len(txt.replace(" ", "")))
+# """doing it in one line"""
+# print("please give us your text: ", end="")
+# txt = input()
+# print(len(txt.replace(" ", "")))
+
+words = []
+for i in range(1, 6):
+    word = input(f"Enter word {i}: ")
+    words.append(word)
+for word in words:
+    print(word,":",len(word))
